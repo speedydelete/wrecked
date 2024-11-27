ccd9a610f5e0a553d952f2f064f9cd0722e4ea0a
speedydelete <speedykorok777@gmail.com>
2024-11-27 15:33:33 -0500

    bunch of bugfixes

--- client.js
+++ client.js
@@ -6,8 +6,12 @@
     const showErrorsOnScreen = false;
 
     const dataURIRegex = /data:([a-zA-Z0-9!#$&.+-^_]+\/[a-zA-Z0-9!#$&.+-^_]+)?(;[a-zA-Z0-9!#$&.+-^_]+)*;base64,[A-Za-z0-9+/]+={0,2}/g;
-    const importRegex = /^(?:(import(?:\s+\*\s+as\s+\w+|\s+\w+|\s*\{[^}]*\})\s+from\s+)["']([^"']+)["'];?)(?:\s*)(?!\/\/\s*wrecked-no-grab=true)\n/m;
+    const importRegex = /^(?:(import(?:\s+\*\s+as\s+\w+|\s+\w+|\s*\{[^}]*\})\s+from\s+)["']([^"']+)["'];?)(?:\s*)(?!\/\/\s*wrecked-no-grab=true)\n/mg;
     const schemeRegex = /^[a-z][a-z\d+\-.]*:/;
+    const pathSep = /(?<!\\)\//;
+    const extTypes = {
+        '.js': 'javascript',
+    };
 
     if (showErrorsOnScreen) {
         window.onerror = function(a, b, c, d, error) {
@@ -15,22 +19,30 @@
             text = text.replace(dataURIRegex, '<data uri>');
             if (document.readyState == 'complete') {
                 document.body.innerHTML = `<pre style="font-size:14px;color:red;padding-left:15px;"></pre>`;
-                document.querySelector('body pre').innerText = text4
+                document.querySelector('body pre').innerText = text;
             } else {
                 alert(text);
             }
             let lastInterval = setInterval(()=>{},0);
-            for (let j=1;j<=i;j++) clearInterval(j)
+            for (let i = 1; i <= lastInterval; i++) clearInterval(i)
         }
         window.onunhandledrejection = function(error) {
-            onerror(null, null, null, null, {stack: e.reason.stack ? e.reason.stack : e.reason});
+            onerror(null, null, null, null, {stack: error.reason.stack ? error.reason.stack : error.reason});
         }
     }
-    
-    const pathSep = /(?<!\\)\//;
+
+    const isFile = window.location.protocol == 'file:';
+
     let fileMap = null;
     let currentPath = null;
     let scripts = [];
+
+    function getExtType(path) {
+        for (const [ext, type] of Object.entries(extTypes)) {
+            if (path.endsWith(ext)) return type;
+        }
+        return 'text';
+    }
 
     function simplifyPath(...paths) {
         let out = [];
@@ -49,90 +61,112 @@
         return '/' + out.filter((x) => x !== '').join('/');
     }
 
-    if (window.location.protocol == 'file:') {
-        const oldFetch = fetch;
-        window.fetch = async function(url) {
-            if (schemeRegex.test(url)) return await oldFetch(url);
-            const path = simplifyPath(url);
-            if (typeof fileMap[path] == 'string') return fileMap[path];
-            let code = await fileMap[path].text();
-            let match = importRegex.exec(code);
-            while (match) {
+    const oldFetch = fetch;
+
+    window.fetch = async function(url) {
+        if (schemeRegex.test(url) || !isFile) {
+            const resp = await oldFetch(url);
+            if (!resp.headers.has('Content-Type')) {
+                resp.headers.set('Content-Type', getExtType(url));
+            }
+            return resp;
+        } else {
+            const file = fileMap[simplifyPath(url)];
+            const code = typeof file == 'string' ? file : await file.text();
+            const headers = {'Content-Type': getExtType(url)};
+            return new Response(code, {status: 200, statusText: 'OK', headers: headers});
+        }
+    }
+
+    async function loadScriptData(script) {
+        if (script.data == undefined) {
+            if (!script.src) return false;
+            const resp = await fetch(script.src);
+            if (!script.type) script.type = resp.headers.get('Content-Type');
+            script.data = await resp.text();
+        }
+        return script;
+    }
+
+    async function patchScript(script) {
+        const type = script.type;
+        let code = script.data;
+        if (type == 'javascript') {
+            let matches = code.matchAll(importRegex);
+            for (const match of matches) {
                 let specifier = match[2];
                 if (!(/^\.?\.?\//.test(specifier))) {
                     specifier = 'https://unpkg.com/' + specifier + '?module';
                 } else {
                     specifier = '_wrecked' + simplifyPath(specifier);
                 }
-                code = code.replace(match[0], match[1] + '"' + specifier + '"; // wrecked-no-grab=true\n');
-                match = importRegex.exec(code);
+                code = code.replaceAll(match[0], match[1] + '"' + specifier + '"; // wrecked-no-grab=true\n');
             }
-            return new Response(code, {status: 200, statusText: 'OK'});
         }
+        return {type: type, data: code};
     }
 
     async function runScript(script) {
-        const {type, src} = script;
-        let {data} = script;
-        if (data == undefined) {
-            if (!src) return;
-            data = await fetch(src);
-        }
-        if (type == 'importmap') {
-            const map = JSON.parse(data);
-            let imports = [];
-            for (const [key, value] of Object.entries(map.imports)) {
-                imports.push([key, await getURL(value)]);
-            }
-            map.imports = Object.fromEntries(imports);
-            data = JSON.stringify(map);
-        }
+        script = await loadScriptData(script);
+        if (!script) return;
+        script = await patchScript(script);
         const elt = document.createElement('script');
-        elt.textContent = data;
+        elt.textContent = script.data;
         elt.setAttribute('data-wrecked-no-grab', 'true');
-        elt.type = type;
+        elt.type = script.type == 'importmap' ? 'importmap' : 'module';
+        console.log('running', elt.textContent.replace(dataURIRegex, '<data uri>'));
         document.body.appendChild(elt);
     }
 
     async function initialRun() {
-        const imports = Object.fromEntries(Object.keys(fileMap).map((k) => ['_wrecked' + k, k]));
-        await runScript({type: 'importmap', data: JSON.stringify({imports: imports})});
-        for (const script of scripts) {
+        const scriptsCopy = scripts.slice();
+        let map;
+        if (isFile) {
+            map = Object.fromEntries(Object.keys(fileMap).map((k) => ['_wrecked' + k, k]));
+        } else {
+            map = {};
+            while (scripts.length > 0) {
+                const script = await loadScriptData(scripts.pop());
+                if (!script) return;
+                script.data.matchAll(importRegex).forEach((match) => {
+                    const specifier = match[2];
+                    if ((/^\.?\.?\//.test(specifier))) {
+                        scripts.push({src: specifier, type: getExtType(specifier)});
+                    }
+                });
+                if (script.src) {
+                    const specifier = (/^\.?\.?\//.test(script.src) ? '_wrecked' : '.') + simplifyPath(script.src);
+                    const patched = await patchScript(await loadScriptData(script));
+                    map[specifier] = 'data:text/javascript;base64,' + btoa(patched.data);
+                }
+            }
+        }
+        await runScript({type: 'importmap', data: JSON.stringify({imports: map})});
+        for (const script of scriptsCopy) {
             await runScript(script);
         }
     }
 
     function interceptScript(script) {
         if (script.tagName && script.tagName == 'SCRIPT' && !script.getAttribute('data-wrecked-no-grab')) {
-            const type = script.getAttribute('type');
+            if ((/^\s+\/\/[^\n]*wrecked-no-grab=true/.test(script.textContent))) return;
+            console.log(script);
+            let type = script.getAttribute('type');
+            if (type.startsWith('wrecked/')) type = type.slice(8);
             script.setAttribute('type', 'text/plain');
-            script.parentNode.removeChild(script)
+            script.parentNode.removeChild(script);
             script.setAttribute('data-wrecked-no-grab', 'true');
-            src = script.getAttribute('src');
+            const src = script.getAttribute('src');
             if (src && src !== '<anonymous code>') {
-                if (window.location.protocol == 'file:') {
-                    scripts.push({type: type, src: src});
-                } else {
-                    fetch(script.src).then(function(resp) {
-                        if (resp.ok) {
-                            resp.text().then((text) => scripts.push({
-                                type: type,
-                                data: text,
-                            }));
-                        } else {
-                            console.error(`Unable to fetch script from ${script.src} due to a ${resp.status} ${resp.statusText} `);
-                        }
-                    });
-                }  
+                scripts.push({type: type, src: src});
             } else {
-                if (!(/^\s+\/\/[^\n]*wrecked-no-grab=true/.test(script.textContent))) return;
                 scripts.push({type: type, data: script.textContent});
             }
         }
     }
-    
+
     document.querySelectorAll('script').forEach(interceptScript);
-    
+
     new MutationObserver(function(mutations) {
         for (const mutation of mutations) {
             for (const node of mutation.addedNodes) {
@@ -162,10 +196,13 @@
             mode: 'request',
             type: 'currentpath',
         }, '*');
+    } else {
+        currentPath = '/';
+        window.addEventListener('load', initialRun);
     }
-    
+
     window.require = async function(path) {
-        const script = await fileMap[resolvePath(path)].text();
+        const script = await fileMap[simplifyPath(path)].text();
         const out = {require: require, module: {exports: {}}};
         Function(`'use strict'; ${script}`).bind(out)();
         return out.module.exports;
